1ebf0041cafa94325e94132877253aedde2c5450
mkmach <mkmach@hotmail.com>
2022-08-30 10:25:27 -0700

    iterating over arrays tinkering

---
+++ packing-list.js
@@ -0,0 +1,5 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cookiing utensils",];
+console.log("Kitchen stuff to pack:");
+for (let i = 0, i < packingList.length; i++) {
+  console.log(packingList);
+}
