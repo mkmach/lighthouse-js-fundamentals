6f51045dd365b5448b0897ac0647dd12da679e24
mkmach <mkmach@hotmail.com>
2022-08-30 12:10:26 -0700

    while loop

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,9 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cookiing utensils",];
+
 console.log("Kitchen stuff to pack:");
-for (let i = 0, i < packingList.length; i++) {
-  console.log(packingList);
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
